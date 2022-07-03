3aa9d39f9030382a9bd3004f9425751e541e953e
peter <peter.alabi@rocketmail.com>
2022-07-03 04:42:25 +0000

    manage cor middleware

---
+++ censusapp/component/Census.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+class Census extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            censuses: []
+        };
+    }
+    componentDidMount() {
+        fetch('http://localhost:8081/api/census', {
+            method:'GET'
+        })
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    this.setState({
+                        isLoaded: true,
+                        censuses: data
+                    });
+                },
+                
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, words } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+
+            {censuses.map((s) => ((<li>{s.city}::{s.census}}</li>)))}
+                </ul>
+
+            );
+        }
+    }
+}
+export default Census;
--- server.js
+++ server.js
@@ -15,7 +15,12 @@ const express = require('express');
 const app = express();
 
 const PORT = 8081;
-
+app.use((req, res, next)=> {
+    res.header('Access-Control-Allow-Origin',"http://localhost:3000")
+    res.header('Access-Control-Allow-Header', 
+                "Origin, X-Request-With Content-Type, Accept");
+    next();
+})
 app.use(express.json());
 app.use("/api", require('./route/request'));
 
